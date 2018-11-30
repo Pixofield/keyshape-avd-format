bfcd0d61f70dbe7c0a4f14a6d6bea194d1a518c6
Pixofield <kari.pihkala@pixofield.com>
2018-11-30 14:14:01 +0200

    Improve slightly android color to svg color conversion

--- importer.js
+++ importer.js
@@ -462,10 +462,14 @@ function androidColorToSvgColor(color, ignoreAlpha)
     if (!color.startsWith("#")) {
         return "#000000";
     }
-    if (color.length == 7) {
+    // #rgb or #rrggbb
+    if (color.length == 4 || color.length == 7) {
         return color;
     }
     if (color.length > 7 && ignoreAlpha) { // if it is "#aarrggbb", then remove alpha value
+        if (color == "#00000000") {
+            return "none";
+        }
         return "#"+color.substring(3);
     }
     // convert to rgba()
